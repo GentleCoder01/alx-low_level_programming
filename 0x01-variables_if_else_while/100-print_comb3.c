873535254e52f411ea97527c52bd1ad2f4354a3b
GentlCoder01 <bellolayinka787@gmail.com>
2022-12-09 15:53:37 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-	int a;
-	int b;
+	int a = '0';
+	int b = '0';
 
-	for (b = 0; b <= 9; b++)
+	for (b = '0'; b <= '9'; b++)
 	{
-		for (a = 0; a <= 9; a++)
+		for (a = '0'; a <= '9'; a++)
 		{
 			if (!((a == b) || (b > a)))
 			{
 				putchar(b);
 				putchar(a);
-				if (!(a == 9 && b == 8))
+				if (!(a == '9' && b == '8'))
 				{
 					putchar(',');
 					putchar(' ');

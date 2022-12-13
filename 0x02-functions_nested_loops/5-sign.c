25289ddd36c331027086f258403ab7066bfa2aee
GentlCoder01 <bellolayinka787@gmail.com>
2022-12-13 13:14:48 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
+ * @n: the number of the sign to be printed
  * Return: 1 is greater than zero
  * 0 of number is zero
  * -1 if number is less than zero

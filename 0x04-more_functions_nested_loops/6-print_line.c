12e0c6c76070bdbf5d19f358a5d4edb327d7d1df
GentlCoder01 <bellolayinka787@gmail.com>
2022-12-15 15:25:47 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}

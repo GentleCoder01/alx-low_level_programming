c747e19709a0eed2f42c4ef48073afede457ae26
GentlCoder01 <bellolayinka787@gmail.com>
2022-12-15 16:48:52 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int n;
 
+	n = 1;
 	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("fizzBuzz");
+			printf("FizzBuzz");
 		}
-		else if (n % 3 === 0)
+		else if (n % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{

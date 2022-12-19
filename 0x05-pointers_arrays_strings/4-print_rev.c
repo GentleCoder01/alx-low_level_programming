648d122c9f96d464d50358ada88db45e11df25c7
GentlCoder01 <bellolayinka787@gmail.com>
2022-12-19 10:44:50 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (s[r] != '\0')
+		r++;
+	for (r = r - 1; r >= 0; r--)
+		_putchar(s[r]);
+
+	_putchar('\n');
+}
